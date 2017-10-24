5a6f7e30771375250df85a44169fbcab50042cbb
Gbossmamen <32278017+Gbossmamen@users.noreply.github.com>
2017-10-24 20:08:23 +0100

    was
    aa

---
+++ GameGitHubTest/Assets/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public Vector3 offset;
+	public Transform target;
+	public bool UseOffSetValues;
+	public float rotationSpeed;
+
+
+
+	// Use this for initialization
+	void Start () {
+		if (!UseOffSetValues) 
+		{
+			offset = target.position - transform.position;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Get X position of the mouse and rotate the target
+		float horizontal = Input.GetAxis("Mouse Y") * rotationSpeed;
+		target.Rotate (0, horizontal, 0);
+
+
+
+		
+	}
+}
